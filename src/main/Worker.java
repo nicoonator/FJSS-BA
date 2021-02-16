c1a2ab52a969be0c22f2effbc1238863dd4be12d
nicoonator <nicoonator@gmail.com>
2021-02-17 00:52:46 +0100

    Renamed

---
+++ src/main/Worker.java
@@ -0,0 +1,15 @@
+package main;
+
+public class Worker {
+
+	private int[] allowedMachines;
+
+	public int[] getAllowedMachines() {
+		return allowedMachines;
+	}
+
+	public void setAllowedMachines(int[] allowedMachines) {
+		this.allowedMachines = allowedMachines;
+	}
+	
+}
