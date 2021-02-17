49f207a93cfffb262ac29c7b26a3c43642fdbf3f
nicoonator <nicoonator@gmail.com>
2021-02-17 15:02:57 +0100

    added Constructor and addTask function

--- src/main/Job.java
+++ src/main/Job.java
@@ -5,6 +5,10 @@ import java.util.ArrayList;
 public class Job {
 	
 	private ArrayList<Task> tasks;
+	
+	public Job() {
+		tasks = new ArrayList<Task>();
+	}
 
 	public ArrayList<Task> getTasks() {
 		return tasks;
@@ -14,4 +18,11 @@ public class Job {
 		this.tasks = tasks;
 	}
 
+	public void addTask(Task t) {
+		ArrayList<Task> temp;
+		temp = this.getTasks();
+		temp.add(t);
+		this.setTasks(temp);
+	
+	}
 }
