6c79e16d66b6ac29a95a4dde6f7b280033c63550
nicoonator <nicoonator@gmail.com>
2021-02-22 02:28:19 +0100

    added Machine Number

--- src/main/Machine.java
+++ src/main/Machine.java
@@ -4,7 +4,15 @@ public final class Machine {
 
 	//alles final
 	//keine setters
-	public Machine() {
-		
+	private final int machineNumber;
+	
+	public Machine(int number) {
+		this.machineNumber = number;
 	}
+
+	public int getMachineNumber() {
+		return machineNumber;
+	}
+	
+	
 }
