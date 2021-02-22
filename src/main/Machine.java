dcfa82ab8aa5dd392e1e91d986f58a7e6f77f375
nicoonator <nicoonator@gmail.com>
2021-02-22 12:13:16 +0100

    added AllowedWorkers in Machine

--- src/main/Machine.java
+++ src/main/Machine.java
@@ -1,17 +1,36 @@
 package main;
 
+import java.util.ArrayList;
+
 public final class Machine {
 
 	//alles final
 	//keine setters
 	private final int machineNumber;
+	private ArrayList<Worker> allowedWorkers;
 	
 	public Machine(int number) {
 		this.machineNumber = number;
+		allowedWorkers = new ArrayList<Worker>();
 	}
 
 	public int getMachineNumber() {
 		return machineNumber;
+	}
+
+	public void addAllowedWorker(Worker worker) {
+		ArrayList<Worker> temp;
+		temp = this.getAllowedWorkers();
+		temp.add(worker);
+		this.setAllowedWorkers(temp);
+	}
+
+	public ArrayList<Worker> getAllowedWorkers() {
+		return allowedWorkers;
+	}
+
+	public void setAllowedWorkers(ArrayList<Worker> allowedWorkers) {
+		this.allowedWorkers = allowedWorkers;
 	}
 	
 	
