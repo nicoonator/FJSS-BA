7438b2c7689c1bd3b8bf27d05e836d0625079535
nicoonator <nicoonator@gmail.com>
2021-02-22 12:53:35 +0100

    methods in new Solver Class

--- src/main/Solver.java
+++ src/main/Solver.java
@@ -2,4 +2,14 @@ package main;
 
 public class Solver {
 
+	public Solution useHeuristik(Solution initialSolution) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Solution createInitialSolution(ProblemDetails problem) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }
