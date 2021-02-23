a07c3af2716e9633b17f47774a7049ed0c95baca
nicoonator <nicoonator@gmail.com>
2021-02-23 10:55:27 +0100

    reworkerd ArrayList[] to List<List>

--- src/main/Solution.java
+++ src/main/Solution.java
@@ -1,23 +1,43 @@
 package main;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Solution {
 	
-	ProblemDetails problem;
-    ArrayList<ScheduledTask>[] scheduledMachines;	
+	private ProblemDetails problem;
+	private List<List<ScheduledTask>> scheduledMachines;	
+	
+	public Solution(ProblemDetails problem) {
+		this.problem=problem;
+		scheduledMachines = new ArrayList<List<ScheduledTask>>(problem.getMachineCount());
+	}
 	
 
 	public void print() {
-		for (int i = 0; i < scheduledMachines.length; i++) {
+		for (int i = 0; i < scheduledMachines.size(); i++) {
 			System.out.println("Machine "+i+":");
-			for (ScheduledTask task : scheduledMachines[i]) {
+			for (ScheduledTask task : scheduledMachines.get(i)) {
 				task.print();
 			}
 		}
 	}
 
-		
-	
 
+	public ProblemDetails getProblem() {
+		return problem;
+	}
+
+
+	public List<List<ScheduledTask>> getScheduledMachines() {
+		return scheduledMachines;
+	}
+
+
+	public void setScheduledMachines(List<List<ScheduledTask>> scheduledMachines) {
+		this.scheduledMachines = scheduledMachines;
+	}
+
+	
+	
 }
